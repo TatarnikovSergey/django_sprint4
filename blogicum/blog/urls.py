9b4a5fed6f1e332e297a198bf88f07585f476b3b
TatarnikovSergey <sg.tatarnikov@yandex.ru>
2024-04-16 22:25:15 +0500

    Форма добавления и редактирования пока без проверки прав

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -9,4 +9,11 @@ urlpatterns = [
     path('posts/<int:post_pk>/', views.post_detail, name='post_detail'),
     path('category/<slug:category_slug>/', views.category_posts,
          name='category_posts'),
+    path('posts/create/', views.PostCreateView.as_view(), name='create_post'),
+    # path('posts/create/', views.post_create, name='create_post'),
+    path(
+        'posts/<int:post_id>/edit/',
+        views.PostEditView.as_view(),
+        name='edit_post'
+    )
 ]
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,6 +1,10 @@
 from django.conf import settings
-from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView
 
+from .forms import PostForm
 from .models import Post, Category
 
 
@@ -33,3 +37,31 @@ def category_posts(request, category_slug):
         slug=category_slug))
     context = {'post_list': post_list, 'category': category}
     return render(request, template_name, context)
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/create.html'
+    success_url = reverse_lazy('blog:index')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# def post_create(request):
+#     template_name = 'blog/create.html'
+#     form = PostForm(request.POST or None, files=request.FILES or None,)
+#     if form.is_valid():
+#         post = form.save(commit=False)
+#         post.author = request.user
+#         post.save()
+#         return redirect('blog:index')
+#     return render(request, template_name, {'form': form})
+
+
+class PostEditView(UpdateView):
+    model = Post
+    form_class = PostForm
+    pk_url_kwarg = 'post_id'
+    template_name = 'blog/create.html'
