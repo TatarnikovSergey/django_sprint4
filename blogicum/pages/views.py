8f37ba2c6dfc4c19a8931c638e69f38902776d0a
TatarnikovSergey <154968776+TatarnikovSergey@users.noreply.github.com>
2024-04-23 12:07:54 +0500

    устранение замечаний после ревью

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from http import HTTPStatus
 
 
 class About(TemplateView):
@@ -15,12 +16,19 @@ class Rules(TemplateView):
 
 
 def page_not_found(request, exception):
-    return render(request, 'pages/404.html', status=404)
+    """Обработка ошибки 404 кастомной страницей."""
+    
+    return render(request, 'pages/404.html', status=HTTPStatus.NOT_FOUND)
 
 
 def csrf_failure(request, reason=''):
-    return render(request, 'pages/403csrf.html', status=403)
+    """Обработка ошибки 403 кастомной страницей."""
+    
+    return render(request, 'pages/403csrf.html', status=HTTPStatus.FORBIDDEN)
 
 
 def server_error(request):
-    return render(request, 'pages/500.html', status=500)
+    """Обработка ошибки 500 кастомной страницей."""
+    
+    return render(request, 'pages/500.html', 
+                  status=HTTPStatus.INTERNAL_SERVER_ERROR)
